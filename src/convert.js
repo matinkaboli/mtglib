fe71421864dc683812e515d28d35046b04ac8692
Matin Kaboli <matinkaboli@aol.com>
2018-07-28 00:29:21 +0430

    convert

--- src/convert.js
+++ src/convert.js
@@ -1,7 +1,24 @@
-module.exports = (file, dest = `${file}-graphql.js`) => {
-  const model = require(file);
+const isObject = require('./is-object');
+const recognizeType = require('./recognize-type');
 
-  const imports = [];
+module.exports = file => {
+  const model = require(file).schema.obj;
 
+  const imports = ['GraphQLObjectType'];
 
+  const fields = {};
+
+  for (const i of Object.keys(model)) {
+    fields[i] = {
+      type: isObject(model[i])
+        ? recognizeType(model[i].type)
+        : recognizeType(model[i]),
+    };
+  }
+
+  console.log(fields);
+
+  let output = `import { ${imports.join(', ')} } from 'graphql';`;
+
+  // console.log(output);
 };
