7d80c19d84f9d9fb210a3c1150fab82bc353ed0e
Matin Kaboli <matinkaboli@aol.com>
2018-07-27 19:44:44 +0430

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,14 @@ module.exports = {
   extends: ['airbnb-base'],
   rules: {
     'no-console': 0,
+    'global-require': 0,
     'arrow-parens': 0,
     'import/extensions': 0,
     'no-confusing-arrow': 0,
     'no-underscore-dangle': 0,
     'import/no-unresolved': 0,
     'no-restricted-syntax': 0,
+    'import/no-dynamic-require': 0,
   },
 };
 
